4b39305f20c862bc1e97d3f54da0e8a8a0bfcc52
nadershoulan <70449899+nadershoulan@users.noreply.github.com>
2021-07-11 23:57:05 +0300

    dd

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
             loop: true,
             margin: 30,
             autoplay: true,
-            autoplayTimeout: 2000,
+            autoplayTimeout: 3000,
             nav: false,
             dots: false,
             responsive: {
@@ -37,3 +37,94 @@ $(document).ready(function() {
 
 
 });
+
+
+/////////////////
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+////// this a scroll down btn script v1
+
+  $(document).ready(function() {
+        $(".down").click(function() {
+            $('html, body').animate({
+                scrollTop: $(".up").offset().top
+            }, 1000);
+        });
+    });
+////end of the script 
+
+////// this a scroll down btn script v2
+  $(document).ready(function() {
+        $(".dd").click(function() {
+            $('html, body').animate({
+                scrollTop: $(".hh").offset().top
+            }, 1000);
+        });
+    });
+////end of the script 
+
+////// this a scroll down btn script v2
+
+  $(document).ready(function() {
+        $(".vision").click(function() {
+            $('html, body').animate({
+                scrollTop: $(".message").offset().top
+            }, 1000);
+        });
+    });
+////end of the script 
+////// this a scroll down btn script v2
+
+  $(document).ready(function() {
+        $(".messaage").click(function() {
+            $('html, body').animate({
+                scrollTop: $(".goal").offset().top
+            }, 1000);
+        });
+    });
+////end of the script 
+////// this a scroll down btn script v2
+
+  $(document).ready(function() {
+        $(".goal").click(function() {
+            $('html, body').animate({
+                scrollTop: $(".word").offset().top
+            }, 1000);
+        });
+    });
+////end of the script 
+////// this a scroll down btn script v2
+
+  $(document).ready(function() {
+        $(".goal").click(function() {
+            $('html, body').animate({
+                scrollTop: $(".word").offset().top
+            }, 1000);
+        });
+    });
+////end of the script 
